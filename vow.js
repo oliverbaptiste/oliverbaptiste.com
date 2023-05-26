acc7383105ffb99c607cf627455e4774d670b121
Oliver Baptiste <ob@oliverbaptiste.com>
2023-05-26 01:13:51 -0400

    Sacrificing bytes on the altar of the Sphinx of Black Quartz

---
+++ vow.js
@@ -0,0 +1,6 @@
+let diety = 'Sphinx #1234567890 of black quartz';
+let invocation = `${diety}, judge my vow`;
+let vow = 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.';
+let supplicant = 'Lorem Ipsum';
+
+console.log(`${invocation}: ${vow}\n—${supplicant}`);
